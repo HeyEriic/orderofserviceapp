3b9c0e655ef4f7b7adacd790ef4e36e33b16dda2
Eric Nascimento <eriicdiiego@hotmail.com>
2024-04-28 14:02:02 -0300

    build: init project

---
+++ src/main/java/com/heyeriic/soservice/SoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.heyeriic.soservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyeriic/soservice/SoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyeriic.soservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
