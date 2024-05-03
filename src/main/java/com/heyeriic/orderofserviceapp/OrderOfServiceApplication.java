b50a18d14d9bbaf56c685bf7febdc0c7e12492b7
Eric Nascimento <eriicdiiego@hotmail.com>
2024-05-03 10:04:16 -0300

    refactor: rename main file

---
+++ src/main/java/com/heyeriic/orderofserviceapp/OrderOfServiceApplication.java
@@ -0,0 +1,11 @@
+package com.heyeriic.orderofserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOfServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderOfServiceApplication.class, args);
+    }
+}
