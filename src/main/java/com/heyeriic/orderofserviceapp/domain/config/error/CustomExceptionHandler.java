15dc6367f83755e43df7745a01bfbef1db8a5a7d
Eric Nascimento <eriicdiiego@hotmail.com>
2024-05-10 15:38:41 -0300

    feat: add custom exception handler class

---
+++ src/main/java/com/heyeriic/orderofserviceapp/domain/config/error/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.heyeriic.orderofserviceapp.domain.config.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getStatus().value(), ex.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error", System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
