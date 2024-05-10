18c532dcac4bd3661c566d821e1902ecbc2eadf3
Eric Nascimento <eriicdiiego@hotmail.com>
2024-05-10 16:08:35 -0300

    feat: add find by email method

--- src/main/java/com/heyeriic/orderofserviceapp/domain/client/ClientRepository.java
+++ src/main/java/com/heyeriic/orderofserviceapp/domain/client/ClientRepository.java
@@ -3,4 +3,5 @@ package com.heyeriic.orderofserviceapp.domain.client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByEmail(String email);
 }
