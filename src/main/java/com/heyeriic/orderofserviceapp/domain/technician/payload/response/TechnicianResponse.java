87e974786ef8aac81aa38ca7b8e149029f44e38b
Eric Nascimento <eriicdiiego@hotmail.com>
2024-05-25 15:14:44 -0300

    feat: add technician response

---
+++ src/main/java/com/heyeriic/orderofserviceapp/domain/technician/payload/response/TechnicianResponse.java
@@ -0,0 +1,40 @@
+package com.heyeriic.orderofserviceapp.domain.technician.payload.response;
+
+public class TechnicianResponse {
+    private Long id;
+    private String name;
+    private String specialty;
+    private String document;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
