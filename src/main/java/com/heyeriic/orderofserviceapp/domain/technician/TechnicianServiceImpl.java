97d739eca1f69ab236e8d0c424d3e4d6bbcc7e23
Eric Nascimento <eriicdiiego@hotmail.com>
2024-06-01 20:00:31 -0300

    refactor: add service annotation in class

--- src/main/java/com/heyeriic/orderofserviceapp/domain/technician/TechnicianServiceImpl.java
+++ src/main/java/com/heyeriic/orderofserviceapp/domain/technician/TechnicianServiceImpl.java
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class TechnicianServiceImpl implements ITechnicianService {
     private final TechnicianRepository technicianRepository;
     private final TechnicianMapper technicianMapper;
